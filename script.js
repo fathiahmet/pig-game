947ed268e83ea4f1d0104e4e97b1ce03ebabd3bf
Fathi Ahmed <fathiahmet9@gmail.com>
2026-07-11 18:25:58 +0300

    Initialization logic and simplify new game reset functionality

--- script.js
+++ script.js
@@ -14,15 +14,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = '0';
-score1El.textContent = '0';
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -84,24 +96,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', function () {
-  // Reset game state
-  scores[0] = 0;
-  scores[1] = 0;
-  currentScore = 0;
-  activePlayer = 0;
-
-  score0El.textContent = '0';
-  score1El.textContent = '0';
-  current0El.textContent = '0';
-  current1El.textContent = '0';
-
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-
-  diceEl.classList.add('hidden');
-  btnRoll.disabled = false;
-  btnHold.disabled = false;
-});
+btnNew.addEventListener('click', init); // Reset game state
