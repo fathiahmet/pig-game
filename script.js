76fc82b2b7fba8f340dd184cdc6aad37764888a3
Fathi Ahmed <fathiahmet9@gmail.com>
2026-07-11 15:11:46 +0300

    event listener for dice roll button

--- script.js
+++ script.js
@@ -9,15 +9,21 @@ score0El.textContent = '0';
 score1El.textContent = '0';
 diceEl.classList.add('hidden');
 
-// const btnRoll = document
-//   .querySelector('.btn--roll')
-//   .addEventListener('click', function () {
-//     // 1. Generating a random dice roll
-//     const dice = Math.trunc(Math.random() * 6) + 1;
+const btnRoll = document
+  .querySelector('.btn--roll')
+  .addEventListener('click', function () {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-//     // 2. Display dice
-//     diceEl.classList.remove('hidden');
-//     diceEl.src = `dice-${dice}.png`;
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-//     // 3. Check for rolled 1: if true, switch to next player
-//   });
+    // 3. Check for rolled 1: if true, switch to next player
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    }
+  });
